ca50f08f646624ca4cc498acca45fd0a931b4d6c
Elias Mawa <elias@emawa.io>
2020-04-12 21:27:39 -0600

    fix file authentication

--- src/middleware/VerifyFileAuthentication.ts
+++ src/middleware/VerifyFileAuthentication.ts
@@ -9,49 +9,64 @@
 
 import config from "../../res/config.json";
 import { TimedJWT } from "../util";
-import { unauthorizedAccess } from "../util/errors";
-import { UserPayload, UserData } from "types";
+import { unauthorizedAccess, noContentToProcess, resourceNotFound } from "../util/errors";
+import { UserPayload, UserData, Metadata } from "types";
+import { ParameterizedContext } from "koa";
 
 import mongoose from "mongoose";
 
 const secret = config.crypt.secret;
 
-export default async (ctx: any, next: any): Promise<void> => {
-	const authentication = (ctx.headers.authorization);
+/**
+ * Validates authorization token and user.
+ */
+export default async (ctx: ParameterizedContext, next: any): Promise<void> => {
+	const token = (ctx.headers.authorization as string).split(' ')[1];
+	const models: { [index: string]: mongoose.Model<any, {}> } = ctx.models;
+	
+	const file_data: Metadata = await models['uploads.metadata']
+	.findOne({ hash: ctx.params.id });
 
-	if(authentication) {
-		const models: { [index: string]: mongoose.Model<any, {}> } = ctx.models;
-		const token = authentication.split(' ')[1];
+	if(file_data == null) {
+		ctx.status = resourceNotFound.status;
+		ctx.body = resourceNotFound;
+	} else {
+		if(token == null) {
+			ctx.status = unauthorizedAccess.status;
+			ctx.body = unauthorizedAccess;
+		} else {
+			try{ 
+				const authorization = TimedJWT.verify(token, secret);
 
-		try {
-			const authorization = TimedJWT.verify(token, secret);
+				if(authorization) {
+					const payload: UserData = authorization.payload;
+		
+					let user: UserData = await models.User
+					.findOne({ username: payload.username });
 
-			if(authorization) {
-				const payload: UserData = authorization.payload;
-
-				let user: UserData = await models.User
-				.findOne({ username: payload.username });
-
-				if(user == null) {
-					ctx.status = unauthorizedAccess.status;
-					ctx.body = unauthorizedAccess;
-				} else {
 					ctx.auth.user = payload.username;
 					ctx.auth.profile = payload.profile;
 					ctx.auth.flags = payload.flags;
 
-					await next();
+					if(file_data.protected == true) {
+						if(file_data.owner == user.username) {
+							await next();
+						} else {
+							ctx.status = unauthorizedAccess.status;
+							ctx.body = unauthorizedAccess;
+						}
+					} else {
+						await next();
+					}
+				} else {
+					ctx.status = unauthorizedAccess.status;
+					ctx.body = unauthorizedAccess;
 				}
-			} else {
+			} catch(err) {
 				ctx.status = unauthorizedAccess.status;
 				ctx.body = unauthorizedAccess;
 			}
-		} catch(err) {
-			ctx.status = unauthorizedAccess.status;
-			ctx.body = unauthorizedAccess;
 		}
-	} else {
-		ctx.status = unauthorizedAccess.status;
-		ctx.body = unauthorizedAccess;
 	}
 };
+
