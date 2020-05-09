8aceed7e9f569621dab5df3f2c932e89bfcf87d1
Elias Mawa <elias@emawa.io>
2020-05-09 16:18:57 -0600

    setup basic file

--- src/controller/api/file.controller.ts
+++ src/controller/api/file.controller.ts
@@ -14,19 +14,19 @@ import fs, { createReadStream, stat } from "fs";
 import crypto from "crypto";
 import path from "path";
 
-import { Connection } from "typeorm";
+import { Connection, Like } from "typeorm";
 import validator from "validator";
 
-import { userNotFound, unauthorizedAccess, actionSuccessful, actionUnsuccessful, serverError, validationError } from "../../util/status";
+import { userNotFound, unauthorizedAccess, actionSuccessful, actionUnsuccessful, serverError, validationError, resourceNotFound } from "../../util/status";
 
-import { MetadataModel } from "../../model/mysql";
-import { UploadSchema } from "../../schema/mysql";
+import { FileTimestampModel } from "../../model/mongo";
+import { MetadataModel, ProfileModel } from "../../model/mysql";
+import { UploadSchema } from "../../schema";
 
-import { jwtIdentify } from "../../middleware";
+import { jwtIdentify, fileAccess } from "../../middleware";
 
 import { bcrypt, TimedJWT } from "../../util";
 import { ProfileData, AuthResponce, Metadata, UploadRequest } from "types";
-import Schema from "../../schema";
 
 import config from "../../../res/config.json";
 
@@ -76,7 +76,7 @@ router.post("/upload", jwtIdentify, async (ctx: ParameterizedContext) => {
 					file_metadata.filename = file.name;
 					file_metadata.type = file.type;
 					file_metadata.bytes = file.bytes;
-					file_metadata.owner = ctx.auth.user;
+					file_metadata.profile_id = ctx.auth.profile_id;
 
 					const p = String(body.protected).toLowerCase() == 'true';
 					const h = String(body.hidden).toLowerCase() == 'true';
@@ -119,7 +119,7 @@ router.post("/upload", jwtIdentify, async (ctx: ParameterizedContext) => {
 	}
 });
 
-// router.post("/upload/url", VerifyIdentity,
+// router.post("/upload/url", jwtIdentify,
 // 	async (ctx: ParameterizedContext) => {
 
 // 	const req: any = ctx.request.body;
@@ -197,7 +197,29 @@ router.post("/upload", jwtIdentify, async (ctx: ParameterizedContext) => {
 // 	}
 // });
 
-router.post("/delete/:id", jwtIdentify, async (ctx: ParameterizedContext) => {
+// router.post("/delete/:id", jwtIdentify, async (ctx: ParameterizedContext) => {
+// 	const body: UploadRequest = ctx.request.body;
+// 	const db: Connection = ctx.mysql;
+	
+// 	const file_repo = db.manager.getRepository(MetadataModel);
+
+// 	const file_path = path.join(config.data_dir, ctx.params.id);
+
+// 	await models['uploads.metadata']
+// 	.updateOne({ file_id: ctx.params.id }, { deleted: true })
+// 	.then(() => {
+// 		agenda.now('QueueFileDelete', { file_id: ctx.params.id, file_path });
+
+// 		ctx.status = resourceQueuefDeleted.status;
+// 		ctx.body = resourceQueuefDeleted;
+// 	})
+// 	.catch(() => {
+// 		ctx.status = serverError.status;
+// 		ctx.body = serverError;
+// 	});
+// });
+
+router.all("/stream/:id", fileAccess, async (ctx: ParameterizedContext) => {
 	const body: UploadRequest = ctx.request.body;
 	const db: Connection = ctx.mysql;
 	
@@ -205,258 +227,285 @@ router.post("/delete/:id", jwtIdentify, async (ctx: ParameterizedContext) => {
 
 	const file_path = path.join(config.data_dir, ctx.params.id);
 
-	await models['uploads.metadata']
-	.updateOne({ file_id: ctx.params.id }, { deleted: true })
-	.then(() => {
-		agenda.now('QueueFileDelete', { file_id: ctx.params.id, file_path });
+	const file_data = await file_repo
+	.findOne({ file_id: ctx.params.id });
 
-		ctx.status = resourceQueuefDeleted.status;
-		ctx.body = resourceQueuefDeleted;
-	})
-	.catch(() => {
-		ctx.status = serverError.status;
-		ctx.body = serverError;
-	});
+	if(!file_data) {
+		ctx.status = resourceNotFound.status;
+		ctx.body = resourceNotFound.message;
+	} else {
+		try {
+			/* check if file exists on filesystem */
+			fs.statSync(file_path);
+		
+			const range = ctx.headers.range;
+			
+			if(range) {
+				const parts = range.replace(/bytes=/, "").split("-");
+				const start = parseInt(parts[0], 10);
+				const end = parts[1] ? parseInt(parts[1], 10) : file_data.bytes - 1;
+				const chunk_size = (end-start) + 1;
+				
+				if(fs.existsSync(file_path)) {
+					const file_stream
+						= fs.createReadStream(file_path, {start, end});
+					
+				{ /* set headers */
+					ctx.response.set("connection", "keep-alive");
+					ctx.response.set("content-type", file_data.type);
+					ctx.response.set("content-length", chunk_size.toString());
+					ctx.response.set("accept-ranges", "bytes");
+					ctx.response.set("content-range",
+					`bytes ${start}-${end}/${file_data.bytes}`);
+					ctx.response.set("connection", "keep-alive");
+					ctx.response.set("content-disposition",
+					"inline; filename=\""+file_data.filename+'"');
+				}
+				
+				ctx.status = 206;
+				ctx.body = file_stream;
+				} else {
+					ctx.status = resourceNotFound.status;
+					ctx.body = resourceNotFound.message;
+				}
+			} else {
+				{ /* update stats */
+					const update_query = {
+						downloads: file_data.downloads ? ++file_data.downloads : 1,
+					}
+					await file_repo
+					.update({ file_id: ctx.params.id }, update_query);
+
+					const file_timestamp_store
+					= ctx.mongo.manager.getRepository(FileTimestampModel);
+
+					const file_timestamp = new FileTimestampModel();
+					file_timestamp.file_id = ctx.params.id;
+					file_timestamp.profile_id = ctx.auth.profile_id;
+
+					const res = await file_timestamp_store
+					.save(file_timestamp)
+					.catch((err) => {
+						ctx.status = serverError.status;
+						ctx.body = serverError.message;
+					});
+					console.log(res);
+					
+				}	
+				
+				{ /* set headers */
+					ctx.response.set("connection", "keep-alive");
+					ctx.response.set("content-length", file_data.bytes.toString());
+					ctx.response.set("content-type", file_data.type);
+					ctx.response.set("content-disposition",
+					"inline; filename=\""+file_data.filename+'"');
+				}
+				
+				if(fs.existsSync(file_path)) {
+					ctx.status = 200;
+					ctx.body = fs.createReadStream(file_path);
+				} else {
+					ctx.status = resourceNotFound.status;
+					ctx.body = resourceNotFound.message;
+				}
+			}
+		} catch(err) {
+			ctx.status = resourceNotFound.status;
+			ctx.body = resourceNotFound.message;
+		}
+	}
 });
 
-// router.all("/stream/:id", VerifyFileAuthentication,
-// 	async (ctx: ParameterizedContext) => {
-
-// 	const req = ctx.request.body;
-// 	const models: { [index: string]: mongoose.Model<any, {}> } = ctx.models;
-
-// 	const file_path = path.join(config.file_store.slow[0], ctx.params.id);
-
-// 	const file_data: Metadata = await models['uploads.metadata']
-// 	.findOne({ file_id: ctx.params.id });
-// 	if(!file_data) { ctx.throw(resourceNotFound.status, resourceNotFound); }
+router.all("/download/:id", fileAccess,
+	async (ctx: ParameterizedContext) => {
 	
-// 	try {
-// 		/* check if file exists on filesystem */
-// 		fs.statSync(file_path);
-// 	} catch(err) {
-// 		ctx.throw(resourceNotFound.status, resourceNotFound);
-// 	}
+	const body: UploadRequest = ctx.request.body;
+	const db: Connection = ctx.mysql;
 
-// 	const range = ctx.headers.range;
+	const file_path = path.join(config.data_dir, ctx.params.id);
 
-// 	if(range) {
-// 		const parts = range.replace(/bytes=/, "").split("-");
-// 		const start = parseInt(parts[0], 10);
-// 		const end = parts[1] ? parseInt(parts[1], 10) : file_data.bytes - 1;
-// 		const chunk_size = (end-start) + 1;
+	const file_repo = db.manager.getRepository(MetadataModel);
+	const file_data = await file_repo.findOne({ file_id: ctx.params.id });
 
-// 		if(fs.existsSync(file_path)) {
-// 			const file_stream = fs.createReadStream(file_path, {start, end});
-			
-// 			{ /* set headers */
-// 				ctx.response.set("connection", "keep-alive");
-// 				ctx.response.set("content-type", file_data.type);
-// 				ctx.response.set("content-length", chunk_size.toString());
-// 				ctx.response.set("accept-ranges", "bytes");
-// 				ctx.response.set("content-range",
-// 				`bytes ${start}-${end}/${file_data.bytes}`);
-// 				ctx.response.set("connection", "keep-alive");
-// 				ctx.response.set("content-disposition",
-// 				"inline; filename=\""+file_data.filename+'"');
-// 			}
-			
-// 			ctx.status = 206;
-// 			ctx.body = file_stream;
-// 		} else {
-// 			ctx.status = resourceNotFound.status;
-// 			ctx.body = resourceNotFound;
-// 		}
-// 	} else {
-// 		{ /* update stats */
-// 			const update_query = {
-// 				downloads: file_data.downloads ? ++file_data.downloads : 1,
-// 			}
-// 			await models['uploads.metadata']
-// 			.updateOne({ file_id: ctx.params.id }, update_query);
+	if(!file_data) {
+		ctx.status = resourceNotFound.status;
+		ctx.body = resourceNotFound.message;
+	} else {
+		if(fs.existsSync(file_path)) {
+			const file_stream = fs.createReadStream(file_path);
 
-// 			const timestamp_store
-// 				= new models['uploads.timestamp']({ file_id: ctx.params.id });
-// 			await timestamp_store.save().catch(() => { });
-// 		}	
+			{ /* update stats */
+				const update_query = {
+					downloads: file_data.downloads ? ++file_data.downloads : 1,
+				}
+				await file_repo
+				.update({ file_id: ctx.params.id }, update_query);
 
-// 		{ /* set headers */
-// 			ctx.response.set("connection", "keep-alive");
-// 			ctx.response.set("content-length", file_data.bytes.toString());
-// 			ctx.response.set("content-type", file_data.type);
-// 			ctx.response.set("content-disposition",
-// 				"inline; filename=\""+file_data.filename+'"');
-// 		}
+				const file_timestamp_store = ctx.mongo.manager.getRepository(FileTimestampModel);
+				const file_timestamp = new FileTimestampModel()
+				file_timestamp.file_id = ctx.params.id;
+				file_timestamp.profile_id = ctx.auth.profile_id;
 
-// 		if(fs.existsSync(file_path)) {
-// 			ctx.status = 200;
-// 			ctx.body = fs.createReadStream(file_path);
-// 		} else {
-// 			ctx.status = resourceNotFound.status;
-// 			ctx.body = resourceNotFound;
-// 		}
-// 	}
-// });
+				file_timestamp_store
+				.save(file_timestamp)
+				.catch((err) => {
+					console.log(err);
+					
+					ctx.status = serverError.status;
+					ctx.body = serverError.message;
+				});;
+			}
 
-// router.all("/download/:id", VerifyFileAuthentication,
-// 	async (ctx: ParameterizedContext) => {
+			ctx.response.set("content-type", file_data.type);
+			ctx.response.set("content-length", file_data.bytes.toString());
+			ctx.response.set("accept-ranges", "bytes");
+			ctx.response.set("connection", "keep-alive");
+			ctx.response.set("content-disposition",
+				"inline; filename=\""+file_data.filename+'"');
 
-// 	const req = ctx.request.body;
-// 	const models: { [index: string]: mongoose.Model<any, {}> } = ctx.models;
+			ctx.body = file_stream;
+		} else {
+			ctx.status = resourceNotFound.status;
+			ctx.body = resourceNotFound;
+		}
+	}
+});
 
-// 	const file_path = path.join(config.file_store.slow[0], ctx.params.id);
-
-// 	const file_data: Metadata = await models['uploads.metadata']
-// 	.findOne({ file_id: ctx.params.id });
-// 	if(!file_data) { ctx.throw(resourceNotFound.status, resourceNotFound); }
-
-// 	if(fs.existsSync(file_path)) {
-// 		const file_stream = fs.createReadStream(file_path);
-
-// 		{ /* update stats */
-// 			const update_query = {
-// 				downloads: file_data.downloads ? ++file_data.downloads : 1,
-// 			}
-
-// 			await models['uploads.metadata']
-// 			.updateOne({ file_id: ctx.params.id }, update_query);
-
-// 			const timestamp_store
-// 				= new models['uploads.timestamp']({ file_id: ctx.params.id });
-// 			await timestamp_store.save().catch(() => { });
-// 		}
-
-// 		ctx.response.set("content-type", file_data.type);
-// 		ctx.response.set("content-length", file_data.bytes.toString());
-// 		ctx.response.set("accept-ranges", "bytes");
-// 		ctx.response.set("connection", "keep-alive");
-// 		ctx.response.set("content-disposition",
-// 			"inline; filename=\""+file_data.filename+'"');
-
-// 		ctx.body = file_stream;
-// 	} else {
-// 		ctx.status = resourceNotFound.status;
-// 		ctx.body = resourceNotFound;
-// 	}
-// });
-
-// router.all("/download/:id/:filename", VerifyFileAuthentication,
-// 	async (ctx: ParameterizedContext) => {
-
-// 	const req = ctx.request.body;
-// 	const models: { [index: string]: mongoose.Model<any, {}> } = ctx.models;
-
-// 	const file_path = path.join(config.file_store.slow[0], ctx.params.id);
-
-// 	const file_data: Metadata = await models['uploads.metadata']
-// 	.findOne({ file_id: ctx.params.id, filename: ctx.params.filename })
-// 	.catch(() => {
-// 		ctx.throw(resourceNotFound.status, resourceNotFound);
-// 	});
-
-// 	if(fs.existsSync(file_path)) {
-// 		const readStream = fs.createReadStream(file_path);
-
-// 		{ /* update stats */
-// 			const update_query = {
-// 				downloads: file_data.downloads ? ++file_data.downloads : 1,
-// 			}
-// 			await models['uploads.metadata']
-// 			.updateOne({ file_id: ctx.params.id }, update_query);
-			
-// 			const timestamp_store
-// 				= new models['uploads.timestamp']({ file_id: ctx.params.id });
-// 			await timestamp_store.save().catch(() => { });
-// 		}
-
-// 		ctx.response.set("content-type", file_data.type);
-// 		ctx.response.set("content-length", file_data.bytes.toString());
-// 		ctx.response.set("accept-ranges", "bytes");
-// 		ctx.response.set("connection", "keep-alive");
-// 		ctx.response.set("content-disposition",
-// 			"inline; filename="+file_data.filename+'"');
-
-// 		ctx.body = readStream;
-// 	} else {
-// 		ctx.status = resourceNotFound.status;
-// 		ctx.body = resourceNotFound;
-// 	}
-// });
-
-// router.all("/info/:id", VerifyFileAuthentication,
-// 	 async (ctx: ParameterizedContext) => {
-
-// 	const req = ctx.request.body;
-// 	const models: { [index: string]: mongoose.Model<any, {}> } = ctx.models;
-
-// 	const file_data: Metadata = await models['uploads.metadata']
-// 	.findOne({ file_id: ctx.params.id });
-// 	if(!file_data) { ctx.throw(resourceNotFound.status, resourceNotFound); }
-
-// 	{ /* update stats */
-// 		const update_query = {
-// 			views: file_data.views ? ++file_data.views : 1,
-// 		}
-// 		await models['uploads.metadata']
-// 		.updateOne({ file_id: ctx.params.id }, update_query);
-// 	}
-
-// 	const responce: MetadataSanitised = {
-// 		file_id: file_data.file_id,
-// 		sha256: file_data.sha256,
-// 		md5: file_data.md5,
-// 		filename: file_data.filename,
-// 		type: file_data.type,
-// 		owner: file_data.owner ? file_data.owner : null,
-// 		protected: file_data.protected,
-// 		hidden: file_data.hidden,
-// 		downloads: file_data.downloads,
-// 		views: file_data.views,
-// 		bytes: file_data.bytes,
-// 		uploaded: file_data.uploaded,
-// 		expires: file_data.expires,
-// 	};
-
-// 	ctx.body = responce;
-// });
-
-// router.all("/search",
-// 	 async (ctx: ParameterizedContext) => {
-
-// 	const req = ctx.request.body;
-// 	const models: { [index: string]: mongoose.Model<any, {}> } = ctx.models;
-
-// 	const limit = req.limit ? parseInt(req.limit) : 15;
-// 	const page = req.page ? parseInt(req.page) : parseInt(ctx.query.page);
-// 	const search_key = req.search_key
-// 		? req.search_key.split(' ').join('|')
-// 		: ctx.query.search_key ? ctx.query.search_key : '';
-
-// 	const query: any = {
-// 		"filename" : {
-// 			$regex: `.*(${search_key}).*`,
-// 			$options: 'i',
-// 		},
-// 		protected: false,
-// 		hidden: false,
-// 	};
-
-// 	if(req.owner) { query.owner = req.owner; }
+router.all("/download/:id/:filename", fileAccess,
+	async (ctx: ParameterizedContext) => {
 	
-// 	const query_data = await new Promise<any>(async (res) => {
-// 		const file_list = await models['uploads.metadata']
-// 		.find(query, { _id: 0, __v: 0 })
-// 		.limit(limit)
-// 		.skip((page - 1) * limit)
-// 		.catch(() => {
-// 			res();
-// 		});
+	const body: UploadRequest = ctx.request.body;
+	const db: Connection = ctx.mysql;
 
-// 		res(file_list);
-// 	});
+	const file_path = path.join(config.data_dir, ctx.params.id);
 
-// 	ctx.body = query_data;
-// });
+	const file_repo = db.manager.getRepository(MetadataModel);
+	const file_data = await file_repo
+	.findOne({ file_id: ctx.params.id, filename: ctx.params.filename });
+
+	if(!file_data) {
+		ctx.status = resourceNotFound.status;
+		ctx.body = resourceNotFound.message;
+	} else {
+		if(fs.existsSync(file_path)) {
+			const file_stream = fs.createReadStream(file_path);
+
+			{ /* update stats */
+				const update_query = {
+					downloads: file_data.downloads ? ++file_data.downloads : 1,
+				}
+				await file_repo
+				.update({ file_id: ctx.params.id }, update_query);
+
+				const file_timestamp_store = ctx.mongo.manager.getRepository(FileTimestampModel);
+				const file_timestamp = new FileTimestampModel()
+				file_timestamp.file_id = ctx.params.id;
+				file_timestamp.profile_id = ctx.auth.profile_id;
+
+				file_timestamp_store
+				.save(file_timestamp)
+				.catch((err) => {
+					console.log(err);
+					
+					ctx.status = serverError.status;
+					ctx.body = serverError.message;
+				});;
+			}
+
+			ctx.response.set("content-type", file_data.type);
+			ctx.response.set("content-length", file_data.bytes.toString());
+			ctx.response.set("accept-ranges", "bytes");
+			ctx.response.set("connection", "keep-alive");
+			ctx.response.set("content-disposition",
+				"inline; filename=\""+file_data.filename+'"');
+
+			ctx.body = file_stream;
+		} else {
+			ctx.status = resourceNotFound.status;
+			ctx.body = resourceNotFound;
+		}
+	}
+});
+
+router.all("/info/:id", fileAccess, async (ctx: ParameterizedContext) => {
+	const body: UploadRequest = ctx.request.body;
+	const db: Connection = ctx.mysql;
+	
+	const file_repo = db.manager.getRepository(MetadataModel);
+	const file_data = await file_repo
+	.findOne({ file_id: ctx.params.id });
+
+	if(!file_data) {
+		ctx.status = resourceNotFound.status;
+		ctx.body = resourceNotFound.message;
+	} else {
+		{ /* update stats */
+			const update_query = {
+				views: file_data.views ? ++file_data.views : 1,
+			}
+			await file_repo
+			.update({ file_id: ctx.params.id }, update_query);
+		}
+
+		const profile_repository = db.manager.getRepository(ProfileModel);
+		
+		const profile_data = await profile_repository
+		.findOne({ profile_id: file_data.profile_id! });
+
+		const responce: Metadata = {
+			file_id: file_data.file_id,
+			sha256: file_data.sha256,
+			md5: file_data.md5,
+			filename: file_data.filename,
+			type: file_data.type,
+			owner: profile_data?.display_name
+				? profile_data.display_name : null,
+			protected: file_data.protected,
+			hidden: file_data.hidden,
+			downloads: file_data.downloads,
+			views: file_data.views,
+			bytes: file_data.bytes,
+			uploaded: file_data.uploaded,
+			expires: file_data.expires,
+		};
+
+		ctx.body = responce;
+	}
+});
+
+router.all("/search", async (ctx: ParameterizedContext) => {
+	const body = ctx.request.body;
+	const db: Connection = ctx.mysql;
+
+	const file_repo = db.manager.getRepository(MetadataModel);
+
+	const limit = body.limit ? parseInt(body.limit) : 15;
+	const page = body.page ? parseInt(body.page) : parseInt(ctx.query.page);
+	const search_key = body.search_key
+		? Like(`%${body.search_key}%`)
+		: Like(`%`);
+
+	const query_data = await new Promise<any>(async (res) => {
+		const file_list = await file_repo
+		.find({
+			where: {
+				filename: search_key,
+				protected: false,
+				hidden: false
+			},
+			select: [ "file_id", "sha256", "md5", "filename", "type", "profile_id", "protected", "hidden", "downloads", "views", "bytes", "uploaded", "expires" ],
+			take: limit,
+			skip: ((page - 1) * limit)
+		})
+		.catch(() => {
+			res();
+		})
+
+		res(file_list);
+	});
+
+	ctx.body = query_data;
+});
 
 const Controller: Router = router;
 
